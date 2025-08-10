adb441c388e0b000e5e73518ecd73ade11144f83
tyavinamua <tyavinamua@63pokupki.ru>
2025-08-10 22:31:05 +0400

    use POM

---
+++ tests/pages/mainPage.spec.ts
@@ -0,0 +1,52 @@
+import { Page, Locator, test, expect } from '@playwright/test';
+import { MainPage } from '../models/MainPage';
+
+let mainPage: MainPage;
+
+test.describe('тесты главной страницы', () => {
+  test.beforeEach(async ({ page }) => {
+   mainPage = new MainPage(page);
+   await mainPage.openMainPage();
+
+  });
+  test('Проверка отображения элементов навигации хедера', async () => {
+    await mainPage.checkElementsVisability();
+  });
+
+  test('Проверка названия элементов навигации хедера', async () => {
+    await mainPage.checkElementsText();
+  });
+
+  test('Проверка атрибутов href элементов навигации хедера', async () => {
+    await mainPage.checkElementsHrefAttribute();
+  });
+
+  test('Проверка переключения лайт мода', async () => {
+    await test.step('Нажатие на иконку переключения лайт мода', async () => {
+      await mainPage.clickSwitchLightModeIcon();
+    });
+    await test.step('Проверка смены значения атрибута', async () => {
+      await mainPage.checkDataThemeAttributeValue();
+    });    
+  });
+
+  test(`Проверка стилей со светлой темой`, async () => {
+    await test.step('Установка светлой темы', async () => {
+      await mainPage.setLightMode();
+    });
+    await test.step('скриншотная проверка с активной светлой темой', async () => {
+      await mainPage.checkLayoutWithLightMode();
+    });    
+  });
+
+  test(`Проверка стилей c темной темой`, async () => {
+    await test.step('Установка темной темы', async () => {
+      await mainPage.setDarkMode();
+    });
+    await test.step('скриншотная проверка с активной темной темой', async () => {
+      await mainPage.checkLayoutWithDarkMode();
+    });  
+    
+    
+  });
+});
